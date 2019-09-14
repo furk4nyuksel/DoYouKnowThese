55fb770d4b970efcddd606189cb60386b54f6072
ibanez75612 <ibanez75612@gmail.com>
2019-09-15 00:47:01 +0300

    Create sqlAdded.sql

---
+++ sqlAdded.sql
@@ -0,0 +1,12 @@
+ 
+Use DoYouNowThese
+
+
+Create Table AppUser(AppUserId int identity primary key,Name nvarchar(100),Surname nvarchar(100),Email nvarchar(200),Password nvarchar(max),ResetKeyCode nvarchar(50),CreateDate datetime  not null,IsActive  bit  not null ,IsDeleted bit not null)
+
+
+Create Table Category(CategoryId int identity primary key,Name nvarchar(100),CategoryImagePath nvarchar(max),CreateDate datetime  not null  ,IsActive bit  not null  ,IsDeleted bit  not null)
+
+Create Table InformationContent(InformationContentId int identity primary key,Title nvarchar(150),Explanation nvarchar(max),PostImagePath nvarchar(max),CategoryId int references Category(CategoryId),AuthorId int references AppUser(AppUserId),LikeCount int not null ,CreateDate datetime    not null,IsActive  bit  not null ,IsDeleted bit  not null )
+
+Create Table InformationReadLog(InformationReadLogId int identity primary key,AppUserId int references AppUser(AppUserId),InformationContentId int references InformationContent(InformationContentId),CreateDate datetime  not null,IsActive bit  not null,IsDeleted bit  not null)
